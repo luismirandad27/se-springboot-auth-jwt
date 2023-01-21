5b70a7e3d1f856b6fce3b19049abe9ac1660a432
Miranda Dulanto <mirandadulantol@student.douglascollege.ca>
2023-01-21 11:18:06 -0800

    Storing roles data once the code starts

--- src/main/java/com/example/tutorial_spring_vue/TutorialSpringVueApplication.java
+++ src/main/java/com/example/tutorial_spring_vue/TutorialSpringVueApplication.java
@@ -1,7 +1,13 @@
 package com.example.tutorial_spring_vue;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.tutorial_spring_vue.model.ERole;
+import com.example.tutorial_spring_vue.model.Role;
+import com.example.tutorial_spring_vue.repository.RoleRepository;
 
 
 @SpringBootApplication
@@ -10,6 +16,16 @@ public class TutorialSpringVueApplication {
 	public static void main(String[] args) {
 		
 		SpringApplication.run(TutorialSpringVueApplication.class, args);
+	}
+	
+	@Bean
+	ApplicationRunner init(RoleRepository roleRepo) {
+		return args -> {
+			roleRepo.save(new Role(ERole.ROLE_ADMIN));
+			roleRepo.save(new Role(ERole.ROLE_MODERATOR));
+			roleRepo.save(new Role(ERole.ROLE_USER));
+		};
+				
 	}
 
 }

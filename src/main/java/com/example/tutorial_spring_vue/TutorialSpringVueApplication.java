170731d472c657caac96913af78692a3d65ec19f
Miranda Dulanto <mirandadulantol@student.douglascollege.ca>
2023-01-21 10:55:44 -0800

    Adding Dependencies for XML Binding

--- src/main/java/com/example/tutorial_spring_vue/TutorialSpringVueApplication.java
+++ src/main/java/com/example/tutorial_spring_vue/TutorialSpringVueApplication.java
@@ -3,10 +3,12 @@ package com.example.tutorial_spring_vue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TutorialSpringVueApplication {
-
+	
 	public static void main(String[] args) {
+		
 		SpringApplication.run(TutorialSpringVueApplication.class, args);
 	}
 
